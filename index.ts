86779d12eeea21cd34b0eec0b152cf45cc451ca4
aaron.haslett <aaron.haslett@gmail.com>
2022-08-08 02:18:51 +1200

    Setting up structure for first POC

--- index.ts
+++ index.ts
@@ -1,1 +1,36 @@
-console.log("it worked");
+enum armorType {
+  unarmored, light, medium, heavy, fortified, hero, divine
+};
+enum attackType {
+  normal, piercing, magic, siege, chaos, hero
+};
+const creatures = [
+  ["ghoul", 13, 340, 0, armorType.heavy, attackType.normal, 1.3],
+  ["footman", 12.5, 420, 2, armorType.heavy, attackType.normal, 1.35],
+];
+
+const creatureSelected = (other: HTMLSelectElement) => (e:Event) => {
+  div.textContent = result;
+};
+
+const sel = document.createElement('select');
+creatures.forEach(([name, ...fields]) => {
+
+  const opt = document.createElement('option');
+  opt.textContent = (name as string);
+  sel.appendChild(opt);
+
+});
+
+const div = document.createElement('div');
+
+window.onload = () => {
+  const rightSel = sel.cloneNode(true);
+
+  sel.addEventListener('change', creatureSelected(rightSel));
+  rightSel.addEventListener('change', creatureSelected(sel));
+
+  document.body.appendChild(sel);
+  document.body.appendChild(rightSel);
+  document.body.appendChild(div);
+};
