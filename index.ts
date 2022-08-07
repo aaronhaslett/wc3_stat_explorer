697ee8d310d4a80a2816afae1792c537b854a273
aaron.haslett <aaron.haslett@gmail.com>
2022-08-08 02:24:32 +1200

    Prettier run

--- index.ts
+++ index.ts
@@ -1,34 +1,43 @@
 enum armorType {
-  unarmored, light, medium, heavy, fortified, hero, divine
-};
+  unarmored,
+  light,
+  medium,
+  heavy,
+  fortified,
+  hero,
+  divine,
+}
 enum attackType {
-  normal, piercing, magic, siege, chaos, hero
-};
+  normal,
+  piercing,
+  magic,
+  siege,
+  chaos,
+  hero,
+}
 const creatures = [
   ["ghoul", 13, 340, 0, armorType.heavy, attackType.normal, 1.3],
   ["footman", 12.5, 420, 2, armorType.heavy, attackType.normal, 1.35],
 ];
 
-const creatureSelected = (other: HTMLSelectElement) => (e:Event) => {
+const creatureSelected = (other: HTMLSelectElement) => (e: Event) => {
   div.textContent = (e.target as HTMLSelectElement).value;
 };
 
-const sel = document.createElement('select') as HTMLSelectElement;
+const sel = document.createElement("select") as HTMLSelectElement;
 creatures.forEach(([name, ...fields]) => {
-
-  const opt = document.createElement('option');
-  opt.textContent = (name as string);
+  const opt = document.createElement("option");
+  opt.textContent = name as string;
   sel.appendChild(opt);
-
 });
 
-const div = document.createElement('div');
+const div = document.createElement("div");
 
 window.onload = () => {
   const rightSel = sel.cloneNode(true) as HTMLSelectElement;
 
-  sel.addEventListener('change', creatureSelected(rightSel));
-  rightSel.addEventListener('change', creatureSelected(sel));
+  sel.addEventListener("change", creatureSelected(rightSel));
+  rightSel.addEventListener("change", creatureSelected(sel));
 
   document.body.appendChild(sel);
   document.body.appendChild(rightSel);
