657d6d28a1317f960abcac265c1dab05be5e8faa
aaron.haslett <aaron.haslett@gmail.com>
2023-01-24 03:27:23 +1300

    Attack strength and DR calculation

--- index.ts
+++ index.ts
@@ -5,41 +5,67 @@ enum armorType {
   heavy,
   fortified,
   hero,
-  divine,
+  //divine,
 }
 enum attackType {
   normal,
   piercing,
   magic,
   siege,
-  chaos,
   hero,
+  chaos,
+  spell,
 }
-const creatures = [
-  ["ghoul", 13, 340, 0, armorType.heavy, attackType.normal, 1.3],
-  ["footman", 12.5, 420, 2, armorType.heavy, attackType.normal, 1.35],
-];
 
-const creatureSelected = (other: HTMLSelectElement) => (e: Event) => {
-  div.textContent = (e.target as HTMLSelectElement).value;
+// Table of (armorType, attackType) => attack strength
+const attackStrength = [
+  [1,1.5,1,1.5,1,1,1],
+  [1,2,1.25,1,1,1,1],
+  [1.5,0.75,0.75,0.5,1,1,1],
+  [1,1,2,1,1,1,1],
+  [0.7,0.35,0.35,.5,0.5,1,1],
+  [1,0.5,0.5,0.5,1,1,0.7],
+]
+
+// name, damage, hp, pop? (food?), armor type, attack type, cooldown
+const creatures: Record<string, number[]> = {
+  "ghoul": [13, 340, 0, armorType.heavy, attackType.normal, 1.3],
+  "footman": [12.5, 420, 2, armorType.heavy, attackType.normal, 1.35],
+};
+
+const c = 0.06;
+
+const creatureSelected = (other: HTMLSelectElement, results: Element) => (e: Event) => {
+  const name = (e.target as HTMLSelectElement).value;
+  const name2 = (other as HTMLSelectElement).value;
+  const [dmg, hp, armor, armor_type, attack_type] = creatures[name];
+  const [dmg2, hp2, armor2, armor_type2, attack_type2] = creatures[name2];
+  const dr = (armor * c)/(1+armor*c);
+  const dr2 = (armor2 * c)/(1+armor2*c);
+  const atk = attackStrength[armor_type2][attack_type];
+  const atk2 = attackStrength[armor_type][attack_type2];
+  const s = `name: ${name} dmg: ${dmg} hp: ${hp} dr: ${dr} atk: ${atk}`;
+  const s2 = `name: ${name2} dmg: ${dmg2} hp: ${hp2} dr: ${dr2} atk: ${atk2}`;
+  results.textContent = s + " " + s2;
 };
 
 const sel = document.createElement("select") as HTMLSelectElement;
-creatures.forEach(([name, ...fields]) => {
+Object.keys(creatures).forEach((name) => {
   const opt = document.createElement("option");
   opt.textContent = name as string;
   sel.appendChild(opt);
 });
 
-const div = document.createElement("div");
+const right_sel = sel.cloneNode(true) as HTMLSelectElement;
 
 window.onload = () => {
-  const rightSel = sel.cloneNode(true) as HTMLSelectElement;
+    const left = document.querySelector(".left");
+    left.appendChild(sel);
 
-  sel.addEventListener("change", creatureSelected(rightSel));
-  rightSel.addEventListener("change", creatureSelected(sel));
+    const right = document.querySelector(".right");
+    right.appendChild(right_sel);
 
-  document.body.appendChild(sel);
-  document.body.appendChild(rightSel);
-  document.body.appendChild(div);
+    const result_div = document.querySelector(".result");
+    sel.addEventListener("change", creatureSelected(right_sel, result_div));
+    right_sel.addEventListener("change", creatureSelected(sel, result_div));
 };
